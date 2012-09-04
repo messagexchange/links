d04255e7d988a6892459cfa5d356bf5cdce2eabe
pvdvreede <paul@vdvreede.net>
2012-09-05 09:31:48 +1000

    Initial commit with top menu for issues.

---
+++ init.rb
@@ -0,0 +1,11 @@
+Redmine::Plugin.register :links do
+  name 'Links plugin'
+  author 'Paul Van de Vreede'
+  description 'Plugin to provide extra hardcoded links around the Redmine menus.'
+  version '1.0.0'
+  url 'https://github.com/messagexchange/links'
+  author_url 'https://github.com/messagexchange'
+
+  # link for top menu to display an issues link to all issues on the site.
+  menu :top_menu, :issues, { :controller => 'issues', :action => 'index'}, :caption => 'Issues'
+end
